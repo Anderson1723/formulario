df7ebe871624342c4f4086cafe66e8fa2e42e7af
Anderson1723 <anderson.amaral3@aluno.ce.gov.br>
2024-05-19 18:04:03 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.getElementById('filter').addEventListener('change', function() {
-    const filterValue = this.value;
-    const items = document.querySelectorAll('.item');
-
-    items.forEach(item => {
-        if (filterValue === 'all') {
-            item.style.display = 'block';
-        } else {
-            if (item.classList.contains(filterValue)) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        }
-    });
+document.querySelector('.back-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'index.html';
 });
